101dc91574b9b4599ef1e1a5bb1def260236cf47
xinxiamu <932852117@qq.com>
2022-09-09 09:57:02 +0800

    好好学习，天天向上

---
+++ Netty-Study/netty-server01/src/main/java/com/nettyserver/nettyserver01/NettyServer01Application.java
@@ -0,0 +1,12 @@
+package com.nettyserver.nettyserver01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyServer01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyServer01Application.class, args);
+	}
+}
---
+++ Netty-Study/netty-server01/src/test/java/com/nettyserver/nettyserver01/NettyServer01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nettyserver.nettyserver01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyServer01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
